83d6f0ef350a68c32ded11fd3ce0337efc0b4884
Mateusz Zajac <TRBC37@motorolasolutions.com>
2018-12-21 15:20:26 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DyrektywyComponent } from './dyrektywy/dyrektywy.component';
+import { BindingComponent } from './binding/binding.component';
+import { FormsModule } from '@angular/forms';
+import { ListaComponent } from './lista/lista.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
+import { AddTaskComponent } from './lista/add-task/add-task.component';
+import { ToDoTaskComponent } from './lista/to-do-task/to-do-task.component';
+import { DoneTaskComponent } from './lista/done-task/done-task.component';
+import { TaskService } from './_services/task.service';
+import { CheckedDirective } from './_common/checked.directive';
+import { DateDirective } from './_common/date.directive';
+import { TranformTaskPipe } from './_common/tranform-task.pipe';
+import { SortNamePipe } from './_common/sort-name.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DyrektywyComponent,
+    BindingComponent,
+    ListaComponent,
+    ParentComponent,
+    ChildComponent,
+    AddTaskComponent,
+    ToDoTaskComponent,
+    DoneTaskComponent,
+    CheckedDirective,
+    DateDirective,
+    TranformTaskPipe,
+    SortNamePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
